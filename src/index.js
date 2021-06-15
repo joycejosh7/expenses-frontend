dda685d1465d9f34f491edc61fad071e085a1081
Joshua <joycejosh7@gmail.com>
2021-06-15 14:57:14 -0400

    added router

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
 import accountReducer from './reducers/accountReducer'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -12,8 +13,10 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = createStore(accountReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-<Provider store={store}>    
-  <App />
+<Provider store={store}>
+  <Router>    
+    <App />
+  </Router>
 </Provider>,
 
 document.getElementById('root')
