0a3059295e603eca4d58daae3e121c8503215c09
Joshua <joycejosh7@gmail.com>
2021-06-21 09:59:55 -0400

    added link to

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,14 +1,19 @@
+
 import React from 'react'
+import { Link} from 'react-router-dom'
+// import Account from './Account'
 
 const Accounts = (props) => {
-    return (
-        <div>
-            {props.accounts.map(account =>
-                <li key={account.id}>
-                    {account.name} - {account.balance}
-                </li>)}
-        </div>
-    )
+
+  return (
+    <div>
+      {props.accounts.map(account =>
+        <li key={account.id}>
+          <Link to={`/accounts/${account.id}`}>{account.name} - ${account.balance}</Link>
+        </li> )}
+    </div>
+
+  )
 }
 
 export default Accounts
