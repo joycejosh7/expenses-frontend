412404a96daf11830a3e2870eb8299efc7f01e2a
Joshua <joycejosh7@gmail.com>
2021-06-21 10:01:39 -0400

    added add and delete transaction reducers

--- src/reducers/accountReducer.js
+++ src/reducers/accountReducer.js
@@ -5,6 +5,24 @@ export default function accountReducer(state = {accounts: []}, action) {
             return {accounts: action.payload}
         case 'ADD_ACCOUNT':
             return {...state, accounts: [...state.accounts, action.payload]}
+        case 'ADD_TRANSACTION':
+            let accounts = state.accounts.map(account => {
+                if (account.id === action.payload.id) {
+                return action.payload
+                } else {
+                return account
+                }
+            })
+            return {...state, accounts: accounts}
+        case 'DELETE_TRANSACTION':
+            let accountsTwo = state.accounts.map(account => {
+            if (account.id === action.payload.id) {
+                return action.payload
+            } else {
+                return account
+            }
+            })
+            return {...state, accounts: accountsTwo}
         default:
             return state
     }
