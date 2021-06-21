34325fa7812a0d0daf98d0abb3769e25daa8d571
Joshua <joycejosh7@gmail.com>
2021-06-21 10:01:12 -0400

    created transactions container

---
+++ src/containers/TransactionsContainer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import TransactionInput from '../components/TransactionInput'
+import Transactions from '../components/Transactions'
+
+class TransactionsContainer extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+          <TransactionInput account={this.props.account}/><br/>
+          <Transactions transactions={this.props.account && this.props.account.transactions}/>
+      </div>
+
+    )
+
+  }
+
+}
+
+export default TransactionsContainer
